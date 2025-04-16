356dbae3ab08b1faaf64aab5df493c10692ed475
Tales Oliveira Bouchardet <t.bouchardet@gmail.com>
2025-04-16 02:37:25 -0300

    atualização

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,13 @@ int main() {
     // Definição das variáveis para armazenar os dados das cartas
     char estado1[3], cidade1[60], codigo1[7];
     int populacao1, nPontosTuristicos1;
-    float area1, pib1, densidadePopulacional1, pibPerCapita1, superPoder1;
+    float area1, densidadePopulacional1, pibPerCapita1, superPoder1;
 
     char estado2[3], cidade2[60], codigo2[7];
     int populacao2, nPontosTuristicos2;
-    float area2, pib2, densidadePopulacional2, pibPerCapita2, superPoder2;
+    float area2, densidadePopulacional2, pibPerCapita2, superPoder2;
+
+    double pib1, pib2;
 
     // Dados para a primeira carta
     printf("\nCadastre as informacoes para a primeira carta:\n-------------------------------------\n\n");
@@ -24,7 +26,7 @@ int main() {
     scanf("%f", &area1);
 
     printf("Digite o PIB da cidade (Carta 1): ");
-    scanf("%f", &pib1);
+    scanf("%lf", &pib1);
 
     printf("Digite o numero de pontos turisticos (Carta 1): ");
     scanf("%d", &nPontosTuristicos1);
@@ -48,7 +50,7 @@ int main() {
     scanf("%f", &area2);
 
     printf("Digite o PIB da cidade (Carta 2): ");
-    scanf("%f", &pib2);
+    scanf("%lf", &pib2);
 
     printf("Digite o numero de pontos turisticos (Carta 2): ");
     scanf("%d", &nPontosTuristicos2);
@@ -61,31 +63,33 @@ int main() {
     superPoder2 = pib2 + area2 + populacao2 + nPontosTuristicos2 + pibPerCapita2 + (1 / densidadePopulacional2);
     
     // Exibicao dos dados da primeira carta
-    printf("\n\nCarta 1:\n--------------------------\n");
-    printf("Codigo: %s\n", codigo1);
-    printf("Estado: %s\n", estado1);
-    printf("Cidade: %s\n", cidade1);
-    printf("Populacao: %d\n", populacao1);
-    printf("Area: %.2f\n", area1);
-    printf("PIB: %.2f\n", pib1);
-    printf("Numero de pontos turisticos: %d\n", nPontosTuristicos1);
-    printf("Densidade populacional: %.2f\n", densidadePopulacional1);
-    printf("PIB per capita: %.2f\n", pibPerCapita1);
+    printf("\n\nCarta 1:\n-------------------------------\n");
+    printf("Codigo................: %s\n", codigo1);
+    printf("Estado................: %s\n", estado1);
+    printf("Cidade................: %s\n", cidade1);
+    printf("Populacao.............: %d\n", populacao1);
+    printf("Area..................: %.2f Km2\n", area1);
+    printf("PIB...................: R$ %.2lf\n", pib1);
+    printf("Pontos turisticos.....: %d\n", nPontosTuristicos1);
+    printf("Densidade populacional: %.2f hab./km2\n", densidadePopulacional1);
+    printf("PIB per capita........: R$ %.2f\n", pibPerCapita1);
+    printf("Super Poder...........: %.2f\n", superPoder1);
 
     // Exibicao dos dados da segunda carta
-    printf("\n\nCarta 2:\n--------------------------\n");
-    printf("Codigo: %s\n", codigo2);
-    printf("Estado: %s\n", estado2);
-    printf("Cidade: %s\n", cidade2);
-    printf("Populacao: %d\n", populacao2);
-    printf("Area: %.2f\n", area2);
-    printf("PIB: %.2f\n", pib2);
-    printf("Numero de pontos turisticos: %d\n", nPontosTuristicos2);
-    printf("Densidade populacional: %.2f\n", densidadePopulacional2);
-    printf("PIB per capita: %.2f\n", pibPerCapita2);
+    printf("\n\nCarta 2:\n-------------------------------\n");
+    printf("Codigo................: %s\n", codigo2);
+    printf("Estado................: %s\n", estado2);
+    printf("Cidade................: %s\n", cidade2);
+    printf("Populacao.............: %d\n", populacao2);
+    printf("Area..................: %.2f Km2\n", area2);
+    printf("PIB...................: R$ %.2lf\n", pib2);
+    printf("Pontos turisticos.....: %d\n", nPontosTuristicos2);
+    printf("Densidade populacional: %.2f hab./km2\n", densidadePopulacional2);
+    printf("PIB per capita........: R$ %.2f\n", pibPerCapita2);
+    printf("Super Poder...........: %.2f\n", superPoder2);
 
     // Comparacao entre os valores das cartas
-    printf("\n\nComparacao entre as cartas:\n--------------------------\n");
+    printf("\n\nComparacao entre as cartas:\n-------------------------------\n");
     printf("Populacao..................: Carta %d venceu (%d)\n", populacao1 > populacao2 ? 1 : 2, populacao1 > populacao2);
     printf("Area.......................: Carta %d venceu (%d)\n", area1 > area2 ? 1 : 2, area1 > area2);
     printf("PIB........................: Carta %d venceu (%d)\n", pib1 > pib2 ? 1 : 2, pib1 > pib2);
--- README.md
+++ README.md
@@ -4,54 +4,59 @@ Exemplo de saída após finalização do script.
 Cadastre as informacoes para a primeira carta:
 -------------------------------------
 
-Digite a populacao da cidade (Carta 1): 500000
-Digite a area da cidade em Km2 (Carta 1): 600  
-Digite o PIB da cidade (Carta 1): 1000000
-Digite o numero de pontos turisticos (Carta 1): 20 
+Digite a populacao da cidade (Carta 1): 434321
+Digite a area da cidade em Km2 (Carta 1): 414240
+Digite o PIB da cidade (Carta 1): 10800251127
+Digite o numero de pontos turisticos (Carta 1): 20
 
 
 Cadastre as informacoes para a segunda carta:
 -------------------------------------
 
-Digite a populacao da cidade (Carta 2): 800000
-Digite a area da cidade em Km2 (Carta 2): 700
-Digite o PIB da cidade (Carta 2): 5000000
-Digite o numero de pontos turisticos (Carta 2): 80
+Digite a populacao da cidade (Carta 2): 2315560  
+Digite a area da cidade em Km2 (Carta 2): 331.4
+Digite o PIB da cidade (Carta 2): 105829675.05
+Digite o numero de pontos turisticos (Carta 2): 70
 
 
 Carta 1:
---------------------------
-Codigo: A01
-Estado: A
-Cidade: Sao Paulo
-Populacao: 500000
-Area: 600.00
-PIB: 1000000.00
-Numero de pontos turisticos: 20
-Densidade populacional: 833.33
-PIB per capita: 2.00
+-------------------------------
+Codigo................: A01
+Estado................: A
+Cidade................: Sao Paulo
+Populacao.............: 434321
+Area..................: 414240.00 Km2
+PIB...................: R$ 10800251127.00
+Pontos turisticos.....: 20
+Densidade populacional: 1.05 hab./km2
+PIB per capita........: R$ 24866.98
+Super Poder...........: 10801124352.00
 
 
 Carta 2:
---------------------------
-Codigo: A02
-Estado: A
-Cidade: Guarulhos
-Populacao: 800000
-Area: 700.00
-PIB: 5000000.00
-Numero de pontos turisticos: 80
-Densidade populacional: 1142.86
-PIB per capita: 6.25
+-------------------------------
+Codigo................: A02
+Estado................: A
+Cidade................: Guarulhos
+Populacao.............: 2315560
+Area..................: 331.40 Km2
+PIB...................: R$ 105829675.05
+Pontos turisticos.....: 70
+Densidade populacional: 6987.21 hab./km2
+PIB per capita........: R$ 45.70
+Super Poder...........: 108145680.00
 
 
 Comparacao entre as cartas:
---------------------------
-Populacao:.................. Carta 2 venceu (0)
-Area:....................... Carta 2 venceu (0)
-PIB:........................ Carta 2 venceu (0)
-Densidade Populacional:..... Carta 1 venceu (1)
+-------------------------------
+Populacao..................: Carta 2 venceu (0)
+Area.......................: Carta 1 venceu (1)
+PIB........................: Carta 1 venceu (1)
+Densidade Populacional.....: Carta 1 venceu (1)
 Numero de Pontos Turisticos: Carta 2 venceu (0)
-PIB per Capita:............. Carta 2 venceu (0)
-Super Poder:................ Carta 2 venceu (0)
+PIB per Capita.............: Carta 1 venceu (1)
+Super Poder................: Carta 1 venceu (1)
+
+
+Pressione ENTER para finalizar...
 ```
